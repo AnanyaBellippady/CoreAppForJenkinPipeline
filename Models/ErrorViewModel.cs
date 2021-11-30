46103ef22c479c87ab6d296ecf46b536f056a1a8
BELLIPPADY <ananya.bellippady@capgemini.com>
2021-11-30 15:04:03 +0530

    adding files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreAppForJenkinPipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
